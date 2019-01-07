d214db34b133081d12216d6f3e2137c8daaf08a8
aswinbinesh <46437407+aswinbinesh@users.noreply.github.com>
2019-01-07 14:06:12 +0530

    Add files via upload

---
+++ encryption-and-decryption.py
@@ -0,0 +1,19 @@
+print("Enter Ant Text:")
+user_text=input()
+plain_text = user_text
+ 
+encrypted_text = ""
+for c in plain_text:
+    x = ord(c)
+    x = x + 1
+    c2 = chr(x)
+    encrypted_text = encrypted_text + c2
+print(encrypted_text)
+ 
+plain_text = ""
+for c in encrypted_text:
+    x = ord(c)
+    x = x - 1
+    c2 = chr(x)
+    plain_text = plain_text + c2
+print(plain_text)
